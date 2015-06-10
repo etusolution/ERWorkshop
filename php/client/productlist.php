7c22175a65100f20ad6834c8f56add4e2568f49d
Indi Chen <indi.chen@gmail.com>
2015-06-10 17:30:48 +0800

    Add the missing productlist.php

---
+++ php/client/productlist.php
@@ -0,0 +1,16 @@
+<?php
+    $product_list = array(
+        "prd_001" => "小紅帽大野狼筆袋",
+        "prd_002" => "小紅帽大野狼零錢包",
+        "prd_003" => "小紅帽大野狼面紙包",
+        "prd_004" => "BELLE CHANTE 飾品木盒",
+        "prd_005" => "HUMANIA 小人萬萬歲迴紋針",
+        "prd_006" => "木匙 – 簡約",
+        "prd_007" => "CONCOMBRE 動物小頭木製夾",
+        "prd_008" => "鄉村白瓷筆筒",
+        "prd_009" => "鄉村奶白小物罐",
+        );
+    $index_row_length = 3;
+    $recommend_length = 4;
+?>
+
