6adcefde57e6017cab7e416d66a4f22332fcdca8
Indi Chen <indi.chen@gmail.com>
2015-06-18 17:38:37 +0800

    Fix resetAccounts tool bug.

--- tools/resetAccounts.sh
+++ tools/resetAccounts.sh
@@ -5,7 +5,7 @@ for accounts in `ls -d user*`; do
 	accountwww=/home/$accounts/www
 	if [ -d $account_www ]; then
 		echo 'Reset' $accountwww
-		rm -rf $account_www
+		rm -rf $accountwww
 	fi
 	cp -rf /root/workshop/www $accountwww
 done
