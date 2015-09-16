ad26c89c4d3488d54fb28dd95208341f68f17f12
Indi Chen <indi.chen@gmail.com>
2015-09-16 14:05:58 +0800

    Correct copyright statement.

--- php/etu/etu_footer.php
+++ php/etu/etu_footer.php
@@ -1,7 +1,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; <a href="http://www.etusolution.com/">Etu taiwan</a> 2015</p>
+                    <p>&copy; 2015 <a href="http://www.etusolution.com/">Etu Corporation</a>. All Rights Reserved.</p>
                 </div>
             </div>
         </footer>
